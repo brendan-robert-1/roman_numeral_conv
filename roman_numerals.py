e0258f864882f5863aace88356a53f7e7d18ea20
Brendan Robert <brendan.robert50point@gmail.com>
2017-07-23 14:25:46 -0400

    More Functionality
    Added the ability to go from roman to a number as well as print all to a file

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,4 +1,5 @@
 import sys
+import webbrowser
 
 #This program is designed to convert integers from 1 to 4999 inclusive into
 #standard roman numerals. There are two modes, either giving the number as a command line
@@ -32,7 +33,7 @@ def less_than_hund(number):
     if number < 40:
         for X in range(int(number / 10)):
             roman_numeral = roman_numeral + 'X'
-    elif number == 40:
+    elif number >= 40 and number < 50:
         roman_numeral = 'XL'
     elif number >= 50 and number < 90:
         roman_numeral = 'L'
@@ -50,10 +51,10 @@ def less_than_thous(number):
     if number >= 1000:
         raise Exception('Number is not less than 1000')
     roman_numeral = ''
-    if number < 500:
+    if number < 400:
         for C in range(int(number/100)):
             roman_numeral = roman_numeral +  'C'
-    elif number == 400:
+    elif number >= 400 and number < 500:
         roman_numeral = 'CD'
     elif number >= 500 and number < 900:
         roman_numeral = 'D'
@@ -85,21 +86,63 @@ def get_roman(number):
     elif number <= 4999:
         return less_than_max(number)
 
+def check_quads(roman):
+    invalid = ['IIII', 'VV','XXXX','LL','CCCC','DD']
+    for n in invalid:
+        if n in roman:
+            raise Exception('Invalid roman numeral entered')
 
-userInput = ''
-try:
-    userInput = sys.argv[1]
-    print(get_roman(int(userInput)))
-except IndexError:
-    while True:
+def get_num(roman):
+    roman = roman.upper()
+    val_map = {'':0, 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    value = 0
+    last_char = ''
+    check_quads(roman)
+    for char in roman:
+        if char not in val_map:
+            raise Exception('Invalid roman numeral entered')
+        if val_map.get(last_char) < val_map.get(char):
+            value += val_map.get(char) - (val_map.get(last_char) + val_map.get(last_char))
+        else:
+            value += val_map.get(char)
+        last_char = char
+    return value
+
+def print_all():
+    with open('roman.txt', 'wb') as output:
+        for i in range(5000):
+            roman = get_roman(i)
+            line = (str(get_num(roman)) + ', ' + roman + '\n').encode()
+            output.write(line)
+        webbrowser.open('roman.txt')
+
+
+
+def main(args):  
+    userInput = ''
+    try:
+        userInput = sys.argv[1]
         try:
-            userInput = int(input('Enter an integer from 1 to 4999 inclusive: '))
-            if userInput <=0 or userInput > 4999:
-                print('Number has to be between 1 and 4999 inclusive.')
-                continue
-            print(get_roman(userInput))
+            i = int(userInput)
+            print(get_roman(i))
         except ValueError:
-            print('Not an integer!')
+            i = str(userInput)
+            print(get_num(i))
+    except IndexError:
+        while True:
+            userInput = input('Enter an integer from 1 to 4999 inclusive or a roman numeral: ')
+            try:
+                userInput = int(userInput)
+                if userInput <=0 or userInput > 4999:
+                    print('Number has to be between 1 and 4999 inclusive.')
+                    continue
+                print(get_roman(userInput))
+            except ValueError:
+                print(get_num(userInput))
+
+main(sys.argv)
+#print_all()
+
 
 
 
